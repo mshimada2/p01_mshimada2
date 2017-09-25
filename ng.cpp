2e946f9ca3f6159a5814f67c6b27e642735c4552
mshimada <mshimada2@mail.csuchico.edu>
2017-09-25 16:29:09 -0700

    Made easy mode interface

---
+++ ng.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+ 
+string difficulty;
+int easyNum;
+int intNum;
+int advNum;
+
+
+    cout<<endl<<"Welcome to the Number Guesser Game!"; 
+        cout<<endl<<"Pick your level of difficulty: Easy, Medium, or Hard";
+
+ cin>> difficulty;
+
+ 
+ 
+ if (difficulty == "easy" || "Easy")
+ 
+        
+
+    cout<<endl<<"You must be a beginner. Correctly guess a number between 1 and 10:";
+        cin>>easyNum; 
+
+ }
+
