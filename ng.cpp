18deecb1c25634f550dcd0a500a89b779d57ce16
mshimada <mshimada2@mail.csuchico.edu>
2017-09-25 22:07:32 -0700

    Game is working with just what is required for the project. Currently attempting to add the embellishments suggested in the project directions

--- ng.cpp
+++ ng.cpp
@@ -14,24 +14,39 @@ int intNum;
 int advNum;
 srand(time(NULL));
 
-int randNum = rand()%10+1;
+int randNum = rand()%1+1;
+int randNum2 = rand()%10+1;
+int randNum3 = rand()%100+1; 
 
     cout<<endl<<"Welcome to the Number Guesser Game! "; 
         cout<<endl<<"Pick your level of difficulty: Easy, Medium, or Hard ";
 
- cin>> difficulty;
+ cin>>difficulty;
 
  
  
  if (difficulty == "easy" || "Easy")
  
+ {
         
 
     cout<<endl<<"You must be a beginner. Correctly guess a number between 1 and 10:  ";
         cin>>easyNum; 
+
+
+
+ }       
+
+
 if (easyNum == randNum)
 
+{
+
+
     cout<<endl<<"You must be a mind reader! ";
+
+
+
 
 else if (easyNum != randNum)
 
@@ -40,4 +55,23 @@ else if (easyNum != randNum)
 
 
  }
+
+
+
+
+
+
+return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
 
